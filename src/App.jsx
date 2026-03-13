f097700c34385daf044af5d6d73ee246906f5aba
Lucas-Lsz <lucas.lsz2004@gmail.com>
2026-03-12 21:07:25 -0400

    tarefa 2 react

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import './App.css'
-import Footer from './components/Footer';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import BemVindo from './components/BemVindo';
+import SecaoHabitos from './components/SecaoHabitos';
+import HabitList from './components/HabitList';
 
 function App() {
+  const habits = [
+    { id: 1, titulo: 'exercício', meta: 5, ativo: true, diasfeitos: 5 },
+    { id: 2, titulo: 'leitura', meta: 7, ativo: true, diasfeitos: 3 },
+    { id: 3, titulo: 'meditação', meta: 7, ativo: false, diasfeitos: 0 },
+    { id: 4, titulo: 'hidratação', meta: 7, ativo: true, diasfeitos: 6 },
+  ]
+
+
   return (
     <>
       <div className="app">
 
         <Header
           titulo="Meus Hábitos"
-          descricao="GeGerencie seus hábitos  diários de forma simples e visual"    
+          //descricao="Gerencie seus hábitos  diários de forma simples e visual"    
         />
-        <BemVindo nomeUsuario="lucas" totalHabitos={5} ></BemVindo>
+        <BemVindo nomeUsuario="lucas" totalHabitos={habits.length} 
+        /> 
+        <secaoHabitos titulo="turma iteam">
+          <HabitList habits={habits} />
+        </secaoHabitos>
         <Footer />
       </div>
     </>
---
+++ src/components/HabitCard.jsx
@@ -0,0 +1,27 @@
+function HabitCard({ titulo, meta, ativo = true, diasfeitos=0, categoria = 'Geral' }) {
+    const metaAtingida = diasfeitos >= meta
+
+    const mensagemMeta = metaAtingida 
+        ? 'Meta da semana atingida!'
+        : `${diasfeitos} de ${meta} dias concluídos.`
+
+  return (
+    <div className= {"Habit-card"}>
+        <h3>{titulo}</h3>
+        <p>{mensagemMeta}</p>
+        <small>categoria: {categoria}</small>
+
+        {/* Ternário: dois resultados possíveis */}
+        <span>{ativo ? 'Ativo' : 'Pausado'}</span>
+
+        {/*&&: um resultadoou nada */}
+        {metaAtingida &&(
+           <p> Parabéns! Você manteve a sequenência essa semana! </p>
+
+        ) }
+
+    </div>
+  );
+}
+
+export default HabitCard;
